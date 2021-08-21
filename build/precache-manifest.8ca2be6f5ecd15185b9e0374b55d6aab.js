1e122d0e8b42dfdb57dcf34c4b66e6b6f8f179bb
suprgyabhushan <Suprgya.56@iiitb.org>
2021-08-21 16:47:23 +0530

    first commit

---
+++ build/precache-manifest.8ca2be6f5ecd15185b9e0374b55d6aab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ab1d7ba8e27eb5545f8",
+    "url": "/ecommerce-react-website/static/css/main.40ce1b11.chunk.css"
+  },
+  {
+    "revision": "2ab1d7ba8e27eb5545f8",
+    "url": "/ecommerce-react-website/static/js/main.d5201b79.chunk.js"
+  },
+  {
+    "revision": "66b5ad36e248a9f1cc12",
+    "url": "/ecommerce-react-website/static/js/runtime~main.4c6a5bc5.js"
+  },
+  {
+    "revision": "cbd333ae05c18ac6ad01",
+    "url": "/ecommerce-react-website/static/js/2.1fdb98ac.chunk.js"
+  },
+  {
+    "revision": "4290bc807f898ade8bc9a4296735ab10",
+    "url": "/ecommerce-react-website/static/media/newLogo.4290bc80.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/ecommerce-react-website/static/media/shopping-bag.09e6f5e7.svg"
+  },
+  {
+    "revision": "1b6ea9897ef25dde68191a37af0d53a0",
+    "url": "/ecommerce-react-website/index.html"
+  }
+];
